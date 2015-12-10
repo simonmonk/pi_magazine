393831bf1209907cee2cfa066c2500c5e63c3571
Simon Monk <Si@Simons-Mac-Pro.local>
2015-12-10 17:04:53 +0000

    added mi_light project

---
+++ 11_mi_light/comp.sh
@@ -0,0 +1,2 @@
+g++ -Ofast -mfpu=vfp -mfloat-abi=hard -march=ARMv7-A -mtune=ARM1176JZF-S -I /usr/local/include -L /usr/local/lib -lrf24-bcm PL1167_nRF24.cpp MiLightRadio.cpp openmili.cpp -o openmilight
+g++ -Ofast -mfpu=vfp -mfloat-abi=hard -march=ARMv7-A -mtune=ARM1176JZF-S -I /usr/local/include -L /usr/local/lib -lrf24-bcm PL1167_nRF24.cpp MiLightRadio.cpp send_cmd.cpp -o send_cmd
